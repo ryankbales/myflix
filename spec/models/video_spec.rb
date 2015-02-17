55595f9de2c91e295f95cd9823444684f2a350fb
Ryan Bales <ryankbales@gmail.com>
2015-02-17 10:44:02 -0800

    adds tests for videos and categories

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,10 @@ describe Video do
 		video.save
 		expect(Video.first).to eq(video)
 	end
+
+	it "belongs to a category" do
+		category = Category.create(name: "Western")
+		video = Video.create(title: 'Dances With Wolves', description: 'Best movie ever!', category_id: category.id)
+		expect(Video.first.category).to eq(category)
+	end
 end
