99a275d486c528b8a83a79d9a63d494aecdf736d
Ryan Bales <ryankbales@gmail.com>
2015-02-17 12:22:30 -0800

    testing validations on videos

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,19 @@ describe Video do
 		video = Video.create(title: 'Dances With Wolves', description: 'Best movie ever!', category_id: category.id)
 		expect(Video.first.category).to eq(category)
 	end
+
+	it "should be valid with title and description" do
+		video =  Video.new(title: 'Dances With Wolves', description: 'Best movie ever!')
+		video.should be_valid
+	end
+
+	it "should not be valid without a title" do
+		video =  Video.new(description: 'Best movie ever!')
+		video.should_not be_valid
+	end
+
+	it "should not be valid without a description" do
+		video =  Video.new(title: 'Dances With Wolves')
+		video.should_not be_valid
+	end
 end
