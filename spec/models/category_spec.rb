03e1ee3355b8e74b23ea33954f43040e381bbc31
Ryan Bales <ryankbales@gmail.com>
2015-02-17 12:43:29 -0800

    adds shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-	it "saves itself" do
-		category = Category.new(name: "Test Category")
-		category.save
-		expect(Category.first).to eq(category)
-	end
-
-	it "has many videos" do
-		category = Category.create(name: "Category")
-		video_1 = Video.create(title: "Video One", description: "This is video one.", category_id: category.id)
-		video_2 = Video.create(title: "Video two", description: "This is video two.", category_id: category.id)
-
-		expect(category.videos).to eq([video_1, video_2])
-	end
+	it { should have_many(:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,30 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-	it "saves itself" do
-		video = Video.new(title: 'Dances With Wolves', description: 'Best movie ever!')
-		video.save
-		expect(Video.first).to eq(video)
-	end
-
-	it "belongs to a category" do
-		category = Category.create(name: "Western")
-		video = Video.create(title: 'Dances With Wolves', description: 'Best movie ever!', category_id: category.id)
-		expect(Video.first.category).to eq(category)
-	end
-
-	it "should be valid with title and description" do
-		video =  Video.new(title: 'Dances With Wolves', description: 'Best movie ever!')
-		video.should be_valid
-	end
-
-	it "should not be valid without a title" do
-		video =  Video.new(description: 'Best movie ever!')
-		video.should_not be_valid
-	end
-
-	it "should not be valid without a description" do
-		video =  Video.new(title: 'Dances With Wolves')
-		video.should_not be_valid
-	end
+	it { should belong_to(:category) }
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:description) }
 end
