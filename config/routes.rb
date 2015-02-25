9944f4d9f9bd237e7c01c7744017ce993610b4e6
Ryan Bales <ryankbales@gmail.com>
2015-02-24 20:50:14 -0800

    wiring up sign in and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Myflix::Application.routes.draw do
+	root to: 'pages#front'
 	get '/home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
 
   resources :videos
   resources :categories
+  resources :users, only: [:new, :create]
 end
