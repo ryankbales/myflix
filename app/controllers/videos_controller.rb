290ee11a7064463663bd2ba72b17de9c107f2dfd
Ryan Bales <ryankbales@gmail.com>
2015-02-24 21:49:44 -0800

    validations implemented

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-
+	before_filter :require_user
 	def index
 		@videos = Video.search_by_title(params[:search])
 		@categories = Category.all
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
 
   resources :videos
   resources :categories
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:create]
 end
