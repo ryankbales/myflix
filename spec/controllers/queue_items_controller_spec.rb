055cde26c37c8a6327862e360ffa6107121978a7
Ryan Bales <ryankbales@gmail.com>
2015-03-07 16:50:47 -0800

    adding a video to the queue

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -17,17 +17,60 @@ describe QueueItemsController do
   end
 
   describe "POST index" do
-    it "redirects to the my queue page" do
-      session[:user_id] = Fabricate(:user).id
-      video = Fabricate(:video)
-      post :create, video_id: video.id
-      expect(response).to redirect_to my_queue_path
+    context "when a user adds a video to the queue" do
+      let(:video) { Fabricate(:video) }
+      it "redirects to the my queue page" do
+        session[:user_id] = Fabricate(:user).id
+        post :create, video_id: video.id
+        expect(response).to redirect_to my_queue_path
+      end
+      it "creates a queue item" do
+        session[:user_id] = Fabricate(:user).id
+        post :create, video_id: video.id
+        expect(QueueItem.count).to eq(1)
+      end
+      it "creates the queue item that is associated with the video" do
+        session[:user_id] = Fabricate(:user).id
+        post :create, video_id: video.id
+        expect(QueueItem.first.video).to eq(video)
+      end
+      it "creates the queue item that is associated with the user" do
+        ryan = Fabricate(:user)
+        session[:user_id] = ryan.id
+        post :create, video_id: video.id
+        expect(QueueItem.first.user).to eq(ryan)
+      end
     end
-    it "creates a queue item"
-    it "creates the queue item that is associated with the video"
-    it "creates the queue item that is associated with the user"
-    it "puts the video as the last video in the queue"
-    it "does not add a duplicate video"
-    it "redirects to the sign in page for unquthenticated users"
+    context "when a queue item is created and there is more than one" do
+      let(:video) { Fabricate(:video) }
+      let(:next_video) { Fabricate(:video) }
+      it "puts the video as the last video in the queue" do
+        ryan = Fabricate(:user)
+        session[:user_id] = ryan.id
+        Fabricate(:queue_item, video_id: video.id, user: ryan)
+        post :create, video_id: next_video.id
+        next_video_queue_item = QueueItem.where(video_id: next_video.id, user_id: ryan.id).first
+        expect(next_video_queue_item.position).to eq(2)
+      end
+    end
+
+    context "when a queue time contains a video that is already in the queue" do
+      let(:video) { Fabricate(:video) }
+      it "does not add a duplicate video" do
+        ryan = Fabricate(:user)
+        session[:user_id] = ryan.id
+        Fabricate(:queue_item, video_id: video.id, user: ryan)
+        post :create, video_id: video.id
+        expect(ryan.queue_items.count).to eq(1)
+      end
+    end
+    
+    context "when a user is unauthenticated" do
+      it "redirects to the sign in page for unauthenticated users" do
+        post :create, video_id: 3
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+    
   end
 end
