e7aca5d75cf9b2c3cfd3a4a57e92d88bb45cf451
Ryan Bales <ryankbales@gmail.com>
2015-03-26 12:49:03 -0700

    a little clean up

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -26,16 +26,19 @@ describe RelationshipsController do
       it_behaves_like "requires sign in" do
         let(:action) {delete :destroy, id: 4}
       end
+
       it "deletes the relationship if the current user is the follower" do
         set_current_user(ryan)
         delete :destroy, id: relationship.id
         expect(Relationship.count).to eq(0)
       end
+
       it "redirects to the people page" do
         set_current_user(ryan)
         delete :destroy, id: relationship.id
         expect(response).to redirect_to people_path
       end
+      
       it "does not delete the relationship if the current user is not the follower" do
         set_current_user(ryan)
         delete :destroy, id: relationship2.id
