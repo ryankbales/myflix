037c52b8bc7557315ebbbefe3b9fa63f30573103
Ryan Bales <ryankbales@gmail.com>
2015-06-01 14:41:02 -0700

    add video url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527174814) do
+ActiveRecord::Schema.define(version: 20150601212701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150527174814) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
