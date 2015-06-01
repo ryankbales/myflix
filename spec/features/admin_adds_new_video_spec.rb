77dc92f4c1d0b295be4de2f5a32c8208c9c7a163
Ryan Bales <ryankbales@gmail.com>
2015-06-01 15:18:46 -0700

    feature specs for admin adding video

---
+++ spec/features/admin_adds_new_video_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'Admin adds new video' do
+  scenario "Admin successfully adds a new video" do
+    admin = Fabricate(:admin)
+    cult = Fabricate(:category, name: "Cult")
+    sign_in(admin)
+    visit new_admin_video_path
+
+    fill_in "Title", with: "Clockwork Orange"
+    select "Cult", from: "Category"
+    fill_in "Description", with: "Freaky movie!"
+    attach_file "Upload Large Cover Image", "spec/support/uploads/donnie_darko_large.jpg"
+    attach_file "Upload Small Cover Image", "spec/support/uploads/donnie_darko_small.jpg"
+    fill_in "Video URL", with: "http://www.iwantthistobedone.com"
+    click_button "Add Video"
+
+    sign_out
+
+    sign_in
+
+    visit video_path(Video.first)
+
+    expect(page).to have_selector("img[src='/uploads/donnie_darko_large.jpg']")
+    expect(page).to have_selector("a[href='http://www.iwantthistobedone.com']")
+  end
+
+end
