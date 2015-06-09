ad18b723acb1ad203368d2ea91d65ba31dcd5aca
Ryan Bales <ryankbales@gmail.com>
2015-06-09 08:29:07 -0700

    one last code edit

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,12 +1,12 @@
-def set_current_user(user=nil)
+def set_current_user(user = nil)
   session[:user_id] = user ? user.id : Fabricate(:user).id
 end
 
-def set_current_admin(admin=nil)
+def set_current_admin(admin = nil)
   session[:user_id] = (admin || Fabricate(:admin)).id
 end
 
-def sign_in(user=nil)
+def sign_in(user = nil)
   user ||= Fabricate(:user)
   visit sign_in_path
   fill_in "Email", with: user.email
