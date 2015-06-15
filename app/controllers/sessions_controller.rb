fa3ba852d5236d7cc62faa6085dfab1662d5c3b1
Ryan Bales <ryankbales@gmail.com>
2015-06-14 21:59:23 -0700

    now I am really almost done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
 	def create
 		user = User.where(email: params[:email]).first
 		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
-			redirect_to home_path, notice: "Welcome #{user.full_name}!"
+			if user.active?
+				session[:user_id] = user.id
+				redirect_to home_path, notice: "Welcome #{user.full_name}!"
+			else
+				flash[:error] = "Your account has been deactivated.  Please give us a shout to fix it."
+				redirect_to sign_in_path
+			end
 		else
 			flash[:errors] = "Invalid email or password."
 			redirect_to sign_in_path
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -3,6 +3,7 @@ Fabricator(:user) do
   password { Faker::Lorem.characters(10) }
   full_name { Faker::Name.name }
   admin false
+  active true
 end
 
 Fabricator(:admin, from: :user) do
