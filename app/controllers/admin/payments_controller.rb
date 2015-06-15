a6b255e49e79a3699102d97bf06e59a5836b7c66
Ryan Bales <ryankbales@gmail.com>
2015-06-14 20:20:26 -0700

    admin can now view payments from stripe with webhook

---
+++ app/controllers/admin/payments_controller.rb
@@ -0,0 +1,6 @@
+class Admin::PaymentsController < ApplicationController
+  before_action :require_admin
+  def index
+    @payments = Payment.all
+  end
+end
---
+++ spec/features/admin_sees_payments_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "Admin sees payments" do
+  background do
+    oscar = Fabricate(:user, full_name: "Oscar Dude", email: "oscar@email.com")
+    Fabricate(:payment, amount: 999, user: oscar)
+  end
+  scenario "admin can see payments" do
+    sign_in(Fabricate(:admin))
+    visit admin_payments_path
+    save_and_open_page
+    expect(page).to have_content("$9.99")
+    expect(page).to have_content("Oscar Dude")
+    expect(page).to have_content("oscar@email.com")
+  end
+
+  scenario "user cannot see payments" do
+    sign_in(Fabricate(:user))
+    visit admin_payments_path
+    save_and_open_page
+    expect(page).not_to have_content("$9.99")
+    expect(page).not_to have_content("Oscar Dude")
+    expect(page).to have_content("You do not have permission to do that.")
+  end
+end
